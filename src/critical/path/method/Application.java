db9ecf8c807c6a0fcc6f4209e3d170c010de4ef3
David León Romero <david110794@gmail.com>
2018-08-04 20:30:45 -0600

    1

--- src/critical/path/method/Application.java
+++ src/critical/path/method/Application.java
@@ -5,12 +5,46 @@
  */
 package critical.path.method;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 /**
  *
  * @author david
  */
 public class Application {
     
+    public void read(String path) throws Exception{
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(new File(path));
+        doc.getDocumentElement().normalize();
+        
+        
+        NodeList as = doc.getElementsByTagName("Actividad");
+        for (int i=0; i<as.getLength(); i++){
+            Node n = as.item(i);
+                if(n.getNodeType() == Node.ELEMENT_NODE){
+                    Element a = (Element) n;
+                    System.out.println("("+a.getAttribute("id") + ", " + Integer.parseInt(a.getAttribute("duracion"))+")");
+                }
+        }
+        as = doc.getElementsByTagName("Relacion");
+        for(int i=0; i<as.getLength(); i++){
+            Node n = as.item(i);
+                if (n.getNodeType() == Node.ELEMENT_NODE){
+                    Element a = (Element) n;
+                    System.out.println(a.getAttribute("actividad") + " ===> " + a.getAttribute("sucesor"));
+                }
+                
+        }
+        
+    }
     /**
      * @param args the command line arguments
      */
